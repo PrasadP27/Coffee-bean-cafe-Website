a12b8e4d51ef979e26624a33509e4e78b7a7b62c
PrasadP27 <bobpanchal948@gmail.com>
2024-02-18 22:41:28 +0530

    Updated Menu Page

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 //for navbar
 window.addEventListener("scroll", function () {
-    var navbar = document.querySelector("header")
-    navbar.classList.toggle("sticky", window.scrollY > 0)
-})
-
+  var navbar = document.querySelector("header");
+  navbar.classList.toggle("sticky", window.scrollY > 0);
+});
 
 //gsap
 
-gsap.from("#about .about-container .about-img", {
-    scrollTrigger: {
-        trigger: '#about .about-container',
-        // markers: true,
-        start: 'top center',
-        end: 'end center',
+//about img
+// gsap.from("#about .about-container .about-img", {
+//     scrollTrigger: {
+//         trigger: '#about .about-container',
+//         // markers: true,
+//         start: 'top center',
+//         end: 'end center',
+//         scrub: true,
+//     },
+//     'border-radius': '0px',
+//     x: '100px',
+//     y: '100px',
+//     opacity: 0,
+// })
+
+
+
+//menu
+let t1 = gsap.timeline()
+
+t1.from('#menu .bg-img', {
+    scrollTrigger:{
+        trigger: "#menu .bg-img",
+        start: '50% 80%',
+        end: '50% 80%',
         scrub: true,
+        // markers: true,
     },
-    'border-radius': '0px',
-    x: '100px',
-    y: '100px',
-    opacity: 0,
+    x: '30%',
 })
