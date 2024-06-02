8839b445e0438cee0c4174c78388405acd32ba80
PrasadP27 <bobpanchal948@gmail.com>
2024-06-03 01:04:44 +0530

    Added funtion for date and time selection

--- public/js/booking.js
+++ public/js/booking.js
@@ -4,3 +4,20 @@ gsap.from('.booking-home h1, .booking-home p , .booking-home nav', {
     stagger: true,
     duration: 1.5,
 });
+
+//flatpicker
+
+flatpickr("#date", {
+    altInput: true,
+    altFormat: "F j, Y",
+    minDate: "today",
+    maxDate: new Date().fp_incr(7) 
+})
+
+flatpickr("#time", {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "h : i  K",
+    minTime: "10:00",
+    maxTime: "23:00"
+})
